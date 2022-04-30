0beb2dc8fa17ebe2b8393ccc886634f8fa012fe8
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-04-30 16:52:11 -0500

    photo schema

---
+++ src/models/photos.js
@@ -0,0 +1,20 @@
+const {Schema, model} = require('mongoose');
+
+
+const PhotoSchema = new Schema({
+    label: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    url: {
+        type: String,
+    }
+
+},
+{
+    versionKey: false,
+    timestamps: true
+})
+
+module.exports = model('Photo', PhotoSchema) 
