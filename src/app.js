37dd76e4c6083249e2a27baa270fb85176e763f8
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-04-30 16:53:09 -0500

    server

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+// dotenv config
+require("dotenv").config();
+
+// mongoose db config
+require("./db.js");
+const indexRouter = require("./router/index.routes");
+const PORT = process.env.PORT || 3000;
+// middleware
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// routes
+app.use("/", indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${port}`);
+});
