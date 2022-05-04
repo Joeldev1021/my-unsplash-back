1117bcddb71212ce51dfeb198991b69caa840de6
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-04 08:24:53 -0500

    feat: route

--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -8,10 +8,8 @@ router.get("/:id", PhotoController.getImageById)
 
 router.post('/', PhotoController.createImage)
 
-/*
 router.put('/:id', PhotoController.updateImage)
 
 router.delete("/:id", PhotoController.deleteImage)
 
- */
 module.exports = router;
