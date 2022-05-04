61d7d218b90828114ca3f5124537d15ab5af9f11
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-03 20:16:28 -0500

    feat: header

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <header className="header">
+          <img src="./logo.svg" alt="logo" />
+      </header>
+  )
+}
+
+export default Header
