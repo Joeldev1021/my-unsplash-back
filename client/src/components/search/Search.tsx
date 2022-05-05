cd141f82ae375781c3c29332d4f5c141242e7a9a
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-05 08:22:14 -0500

    feat: component search

---
+++ client/src/components/search/Search.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.scss";
+interface Props {
+  handleSearch: (value: string) => void;
+}
+
+const Search:React.FC<Props> = ({handleSearch}) => {
+  return (
+    <div className="header-search">
+      <img src="./search.svg" alt="search" />
+      <input onChange={(e)=> handleSearch(e.target.value)} placeholder="Search" type="text" />
+    </div>
+  );
+};
+
+export default Search;
