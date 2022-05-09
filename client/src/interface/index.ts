00e3ef6e572faa6dcc1776add8526a4524cec636
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-09 09:36:44 -0500

    interface PhotoState PhotoContextProps

--- client/src/interface/index.ts
+++ client/src/interface/index.ts
@@ -1,5 +1,15 @@
-export interface PhotoInterface {
+export interface Photo {
   _id?: string;
   url: string;
   label: string;
+}
+
+export interface PhotoState {
+  photos: Photo[];
+}
+
+
+export type PhotoContextProps = {
+  photos: Photo[];
+  addPhoto: (photo: Photo) => void;
 }
