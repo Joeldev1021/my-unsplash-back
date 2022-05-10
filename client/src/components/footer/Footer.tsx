3f1c52b986409ccccbfdd0b4daeadbee5635dbfd
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-10 08:07:06 -0500

    feat: footer

---
+++ client/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.scss'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>Created by: Joel Dev </p>
+    </footer>
+  )
+}
+
+export default Footer
