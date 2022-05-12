fd48d33fdd36bf1fa479b9315fd11fdd9f7d77e0
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-12 08:09:30 -0500

    feat: list photo

--- client/src/components/ListPhoto/ListPhoto.tsx
+++ client/src/components/ListPhoto/ListPhoto.tsx
@@ -1,13 +1,31 @@
-import React from "react";
-import { PhotoInterface } from "../../interface";
-import "./index.scss"
+import React, { useContext, useEffect, useState } from "react";
+import { PhotoContext } from "../../context/PhotoContext";
+import { Photo } from "../../interface";
+import ModalPassword from "../modal/ModalPassword";
+import "./index.scss";
 
-const ListPhoto: React.VFC<PhotoInterface> = ({ url, label }) => {
+interface Props {
+  photo: Photo;
+}
+
+const ListPhoto: React.FC<Props> = ({ photo }) => {
+  const [alertDelete, setAlertDelete] = useState(false);
+
+  const handleDelete = (id?:string) => {
+    setAlertDelete(true);
+  };
+
   return (
-    <div className="card">
-      <img className="card-img" src={url} alt={label} />
-      <p className="card-label">{label}</p>
-    </div>
+    <>
+      <div className="card">
+        <button onClick={() => handleDelete(photo!._id)} className="card-btn">
+          Delete
+        </button>
+        <img className="card-img" src={photo.url} alt={photo.label} />
+        <p className="card-label">{photo.label}</p>
+      </div>
+      {alertDelete && <ModalPassword type="delete" id={photo!._id} setAlertDelete={setAlertDelete}  />}
+    </>
   );
 };
 
