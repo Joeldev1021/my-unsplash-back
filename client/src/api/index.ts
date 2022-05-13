7d3f86b70cbf9cabc4466d32918f0a754264da4b
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-13 10:07:23 -0500

    function load photo, delete photo and create photo

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,15 +1,32 @@
-import { PhotoInterface } from "../interface";
+import { Photo } from "../interface";
 
-const createNewPhoto = async (photo: PhotoInterface) => {
+
+const loadPhotosApi = async () => {
+    const res = await fetch('http://localhost:4000');
+    const photos = await res.json();
+    return photos;
+}
+
+const createNewPhoto = async (photo: Photo) => {
     const res = await fetch('http://localhost:4000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(photo)
-
     });
     return await res.json();
 }
 
-export { createNewPhoto };
+
+const deletePhotoApi = async (id: string ) => {
+    const res = await fetch(`http://localhost:4000/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+    return await res.json();
+}
+
+export { createNewPhoto , loadPhotosApi, deletePhotoApi };
