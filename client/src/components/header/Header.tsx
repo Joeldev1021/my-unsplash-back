65f3c30db7a86b934f1ffb7f4778160c56a4ed72
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-13 10:08:44 -0500

    fix: modal create in header

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -3,22 +3,32 @@ import Search from "../search/Search";
 import "./index.scss";
 
 import { Photo } from "../../interface";
+import ModalCreate from "../modal/ModalCreate";
 
 interface Props {
-  handleSearch: (value: string) =>void;
+  handleSearch: (value: string) => void;
   setIsModalOpen: (value: boolean) => void;
+  isModalOpen: boolean;
 }
 
-const Header:React.FC<Props> = ({handleSearch, setIsModalOpen})  => {
- 
+const Header: React.FC<Props> = ({ handleSearch, setIsModalOpen, isModalOpen }) => {
   return (
-    <header className="header">
-      <div style={{"display": "flex"}}>
-        <img src="./logo.svg" alt="logo" />
-        <Search handleSearch={handleSearch}/>
-      </div>
-      <button onClick={() => setIsModalOpen(true)} className="header-btn">Add Photo</button>
-    </header>
+    <>
+      <header className="header">
+        <div style={{ display: "flex" }}>
+          <img className="header-logo" src="./logo.svg" alt="logo" />
+          <Search handleSearch={handleSearch} />
+        </div>
+        <button onClick={() => setIsModalOpen(true)} className="header-btn">
+          Add Photo
+        </button>
+      </header>
+       {isModalOpen && (
+        <ModalCreate
+          setIsModalOpen={setIsModalOpen}
+        />
+      )} 
+    </>
   );
 };
 
