8a9b656a4a3519b9f68c0d4ba01becf88f2b5234
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-13 10:09:09 -0500

    fix: modalPassword initial

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,41 @@
 import { useContext, useEffect, useState } from "react";
 import "./App.scss";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import ListPhoto from "./components/ListPhoto/ListPhoto";
-import ModalCreate from "./components/modal/ModalCreate";
+import ModalPassword from "./components/modal/ModalPassword";
 import { PhotoContext } from "./context/PhotoContext";
 import { Photo} from "./interface";
-
 
 function App() {
   const [dataFilter, setDataFilter] = useState<Photo[] | null>(null)
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const { photos } = useContext(PhotoContext);
+  const [isModalPasswordOpen, setIsModalPasswordOpen] = useState<boolean>(false);
+  const { photos, password } = useContext(PhotoContext);
 
   useEffect(() => {
     setDataFilter(photos); 
-  }, [photos])
+  }, [photos!]);
 
   const handleSearch = (value: string) => {
      setDataFilter(photos.filter(photo => photo.label.includes(value))) 
   }
-  
+   
   return (
     <div className="App">
-      <Header setIsModalOpen={setIsModalOpen} handleSearch={handleSearch} />
+      <Header setIsModalOpen={setIsModalOpen} handleSearch={handleSearch} isModalOpen={isModalOpen} />
       <div className="wrapper-photo">
         {dataFilter &&
           dataFilter?.map((item: Photo) => {
             return (
-              <ListPhoto key={item._id} label={item.label} url={item.url} />
+              <ListPhoto key={item._id} 
+               photo={item} 
+                />
             );
           })}
       </div>
-      {isModalOpen && <ModalCreate setDataFilter={setDataFilter}  setIsModalOpen={setIsModalOpen} />}     
+      {password.length < 1 && <ModalPassword  type="create-password" />}
+      <Footer />
    </div>
   );
 }
